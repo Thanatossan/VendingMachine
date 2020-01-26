9ff6990613c4f95928e29d97952db48555af62af
Thanatossan <kotokikung@gmail.com>
2020-01-27 02:29:35 +0700

    Updates

---
+++ precache-manifest.31ea8641345ce8106d1f3ce1546dadac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c57d5d6d19b395659bb2d4eeda2731a5",
+    "url": "/VendingMachine/index.html"
+  },
+  {
+    "revision": "f8c1988eeb4664071265",
+    "url": "/VendingMachine/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "64943ddcbab58c6ac665",
+    "url": "/VendingMachine/static/css/main.8c5d14d6.chunk.css"
+  },
+  {
+    "revision": "f8c1988eeb4664071265",
+    "url": "/VendingMachine/static/js/2.62efcc84.chunk.js"
+  },
+  {
+    "revision": "64943ddcbab58c6ac665",
+    "url": "/VendingMachine/static/js/main.f6680b67.chunk.js"
+  },
+  {
+    "revision": "048624adc16a101d075d",
+    "url": "/VendingMachine/static/js/runtime-main.b8398b8a.js"
+  }
+]);
